6284011ce899076be1004482a1dc29d1aa5d15c9
layla <layla_adebutu@biswashington.org>
2025-02-08 20:32:52 +0000

    update

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 print("You wake up with your head pounding, yet no light provides an explanation for why. Your memory doesn't help either - last thing you remember you were sitting in your room. Now you're sitting...well you don't really know where.")
 print("WHAT DO YOU DO?")
-print("inspect your surroundings OR keep sitting")
+print("inspect your surroundings OR stay sitting")
 
-with open ("surroundings.txt" , 'r') as file:
+with open ('surroundings.txt' , 'r') as file:
     surroundings = file.readlines()
 
-with open ("sitting1.txt" , 'r') as file:
+with open ('sitting.txt' , 'r') as file:
     sitting = file.readlines()
 
-answer = input()
-if input in surroundings:
-    print("You look blah blah blah")
-elif input in sitting:
-    print("You stay sitting")
-else:
-    print("That doesn't sound right...try again")
+    answer = input()
+    if answer+"\n" in surroundings:
+        print("You look blah blah blah")
+    elif answer+"\n" in sitting:
+        print("You stay sitting")
+    else:
+        print("That doesn't sound right...try again")
+        print("WHAT DO YOU DO?")
+        print("inspect your surroundings OR stay sitting")
