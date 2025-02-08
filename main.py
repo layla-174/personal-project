348088d3a2fd4b03fe4e5810a90e5ee83fe767e2
layla <layla_adebutu@biswashington.org>
2025-02-08 20:44:45 +0000

    update

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-print("You wake up with your head pounding, yet no light provides an explanation for why. Your memory doesn't help either - last thing you remember you were sitting in your room. Now you're sitting...well you don't really know where.")
-print("WHAT DO YOU DO?")
-print("inspect your surroundings OR stay sitting")
+print("Welcome to 'THE ROOM.' To play, type 'play'. To save progress, type 'save'")
+answer = input()
+play = ["play", "start playing"]
+save = ["save", "save progress"]
 
-with open ('surroundings.txt' , 'r') as file:
-    surroundings = file.readlines()
+if answer in play:
+    print("You wake up with your head pounding, yet no light provides an explanation for why. Your memory doesn't help either - last thing you remember you were sitting in your room. Now you're sitting...well you don't really know where.")
+    print("WHAT DO YOU DO?")
+    print("inspect your surroundings OR stay sitting")
 
-with open ('sitting.txt' , 'r') as file:
-    sitting = file.readlines()
+    with open ('surroundings.txt' , 'r') as file:
+        surroundings = file.readlines()
 
-    answer = input()
-    if answer+"\n" in surroundings:
-        print("You look blah blah blah")
-    elif answer+"\n" in sitting:
-        print("You stay sitting")
-    else:
-        print("That doesn't sound right...try again")
-        print("WHAT DO YOU DO?")
-        print("inspect your surroundings OR stay sitting")
+    with open ('sitting.txt' , 'r') as file:
+        sitting = file.readlines()
+
+        answer = input()
+        if answer+"\n" in surroundings:
+            print("You look blah blah blah")
+        elif answer+"\n" in sitting:
+            print("You stay sitting")
+        elif answer in save:
+            print("Your progress has been saved.")
+        else:
+            print("That doesn't sound right...try again")
+            print("WHAT DO YOU DO?")
+            print("inspect your surroundings or stay sitting")
+
+elif answer in save:
+            print("Your progress has been saved.")
+else:
+    print("That doesn't sound right...try again")
+    print("Welcome to 'THE ROOM.' To play, type 'play'. To save progress, type 'save'")
