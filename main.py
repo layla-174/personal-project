c68f9200547c1076422f0f877eb48fa8a0b0cf97
layla <layla_adebutu@biswashington.org>
2025-02-13 16:19:43 +0000

    update

--- main.py
+++ main.py
@@ -23,8 +23,23 @@ if answer in play:
             with open ('batteries.txt', 'r') as file:
                 batteries = file.readlines()
             if answer+"\n" in batteries:
-                print("You continue to feel")
-
+                print("You decide to look for the batteries of the flashlight, which in hindsight seems pretty futile. Just as you're about to give up, two metal cylinders almost miraculously make their way into your hand. ")
+            with open ('lightsource.txt', 'r') as file:
+                lightsource = file.readlines()
+            elif answer+"\n" in lightsource:
+                print("You discard the useless flashlight, and continue to rely on your other senses to find a source of light. Due to the darkness, you bump into a wall, and - despite your initial irritation - the feeling of what seems to be a light switch subdues your anger. Though, you hesitate, thinling it's a trap. You have a gut feeling telling you to go with the safer option.")
+                print("WHAT DO YOU DO?")
+                print("turn on the light OR go back to the flashlight")
+            else:
+                print(idk)
+            with open ('lighton.txt', 'r') as file:
+                lighton = file.readlines()
+            if answer+"\n" in lighton:
+                print("placeholder")
+            elif answer+"\n" in batteries:
+                 print("You decide to look for the batteries of the flashlight, which in hindsight seems pretty futile. Just as you're about to give up, two metal cylinders almost miraculously make their way into your hand. ")
+             else:
+                print("idk")   
 
         elif answer+"\n" in sitting:
             print("You stay sitting")
