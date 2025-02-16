f06b83fdb05ae97d528186a6dab84445ef6c8eee
layla <layla_adebutu@biswashington.org>
2025-02-16 01:08:52 +0000

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Welcome to 'THE ROOM.' To play, type 'play'. To save progress, type 'save'")
+print("Welcome to 'ESCAPE'. The aim of the game is in the title - you must escape. You will be given choices that will determine whether or not you succeed in escaping. In some circumstances you will be given a 'FATE OR LUCK' question, where a random choice will also determine the outcome. Type 'play' to begin.")
 answer = input()
-play = ["play", "start playing"]
+play = ["play", "start playing", "PLAY"]
 save = ["save", "save progress"]
 
 if answer in play:
@@ -10,7 +10,6 @@ if answer in play:
 
     with open ('surroundings.txt' , 'r') as file:
         surroundings = file.readlines()
-
     with open ('sitting.txt' , 'r') as file:
         sitting = file.readlines()
 
@@ -20,25 +19,38 @@ if answer in play:
             print("WHAT DO YOU DO?")
             print("try to find batteries OR look for a different light source")
             answer = input()
+            
             with open ('batteries.txt', 'r') as file:
                 batteries = file.readlines()
-            if answer+"\n" in batteries:
-                print("You decide to look for the batteries of the flashlight, which in hindsight seems pretty futile. Just as you're about to give up, two metal cylinders almost miraculously make their way into your hand. ")
             with open ('lightsource.txt', 'r') as file:
                 lightsource = file.readlines()
+
+            if answer+"\n" in batteries:
+                print("You decide to look for the batteries of the flashlight, which in hindsight seems pretty futile. Just as you're about to give up, two metal cylinders almost miraculously make their way into your hand. As you turn on the flashlight, you finally see where you are.")
+                print("FATE OR LUCK?")
+                print("choose a whole number between 1-3")
+            with open ('room1.txt', 'r') as file:
+                library = file.readlines()
+            with open ('room2.txt', 'r') as file:
+                cellar = file.readlines()
+            with open ('room3.txt', 'r') as file:
+                bedroom = file.readlines()
+            
             elif answer+"\n" in lightsource:
                 print("You discard the useless flashlight, and continue to rely on your other senses to find a source of light. Due to the darkness, you bump into a wall, and - despite your initial irritation - the feeling of what seems to be a light switch subdues your anger. Though, you hesitate, thinling it's a trap. You have a gut feeling telling you to go with the safer option.")
                 print("WHAT DO YOU DO?")
                 print("turn on the light OR go back to the flashlight")
             else:
-                print(idk)
+                print("idk")
+          
             with open ('lighton.txt', 'r') as file:
                 lighton = file.readlines()
             if answer+"\n" in lighton:
                 print("placeholder")
-            elif answer+"\n" in batteries:
-                 print("You decide to look for the batteries of the flashlight, which in hindsight seems pretty futile. Just as you're about to give up, two metal cylinders almost miraculously make their way into your hand. ")
-             else:
+            #elif answer+"\n" in batteries:
+                 #print("You decide to look for the batteries of the flashlight, which in hindsight seems pretty futile. Just as you're about to give up, two metal cylinders almost miraculously make their way into your hand. ")
+                 #find out how to go back 
+            else:
                 print("idk")   
 
         elif answer+"\n" in sitting:
